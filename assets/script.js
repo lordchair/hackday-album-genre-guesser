a927ec35944498a2285abe2653a90d623f3fad94
Yale Thomas <ythomas@pandora.com>
2017-05-25 09:39:04 -0700

    drag image files onto camera preview to upload

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,34 @@
+
+window.uid_counter = 1;
+function get_uid(base) {
+  window.uid_counter = window.uid_counter + 1;
+  return `base_${window.uid_counter}`;
+}
+
+function process_data_uri(data_uri) {
+  const id = get_uid('image');
+
+  $('#processed_images').prepend($(`<div class="result_wrapper" id="${id}"><img src="${data_uri}"/><div class='loader'/></div>`))
+
+  let formData = new FormData();
+  formData.append('image', data_uri);
+
+  Webcam.upload(data_uri, '/predictGenre', function(code, text) {
+    results = text.split(',');
+    $results = $('<ol class="predictions" />');
+    console.log(`results: ${results}`);
+    for (result in results) {
+      $results.append($('<li>' + results[result] + '</li>'));
+    }
+    $(`#${id} .loader`).remove();
+    $('#'+id).append($results);
+  });
+}
+
+function take_snapshot() {
+  Webcam.snap(process_data_uri);
+}
+
 function initWebcam() {
   Webcam.set({
     // live preview size
@@ -20,37 +51,30 @@ function initWebcam() {
   Webcam.attach( '#my_camera' );
 }
 
-function init() {
-  initWebcam();
-}
+function initDragDrop() {
+  var drag_target = $('#my_camera')
+  .bind( 'dragenter dragover', false)
+  .bind( 'drop', function( e ) {
+    e.stopPropagation();
+    e.preventDefault();
 
-window.uid_counter = 1;
-function get_uid(base) {
-  window.uid_counter = window.uid_counter + 1;
-  return `base_${window.uid_counter}`;
-}
 
-function take_snapshot() {
-  Webcam.snap(function(data_uri) {
-    const id = get_uid('image');
-
-    $('#processed_images').append($(`<div class="result_wrapper" id="${id}"><img src="${data_uri}"/></div>`))
-
-    let formData = new FormData();
-    formData.append('image', data_uri);
-
-    Webcam.upload(data_uri, '/predictGenre', function(code, text) {
-      results = text.split(',');
-      $results = $('<ol class="predictions" />');
-      console.log(`results: ${results}`);
-      for (result in results) {
-        $results.append($('<li>' + results[result] + '</li>'));
-      }
-      $('#'+id).append($results);
-    });
+    $.each( e.originalEvent.dataTransfer.files, function(index, file){
+      var fileReader = new FileReader();
+      fileReader.onload = (function(file) {
+       return function(e) {
+        process_data_uri(e.target.result);
+       };
+     })(file);
+     fileReader.readAsDataURL(file);
+   });
   });
 }
 
+function init() {
+  initWebcam();
+  initDragDrop();
+}
 
 /*
 function guessGenre() {
